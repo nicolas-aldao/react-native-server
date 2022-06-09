5b0e3cfe74f241911ac43b2e7e2fd8c37b701114
Carlos Nicolás Aldao <caldao@baufest.com>
2022-06-09 15:55:14 -0300

    Updates

--- config.js
+++ config.js
@@ -1,1 +1,29 @@
 "use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.config = void 0;
+
+var _dotenv = require("dotenv");
+
+(0, _dotenv.config)();
+var config = {
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.PASSWORD || '',
+  database: process.env.DB || 'test',
+  ssl: {
+    rejectUnauthorized: false
+  }
+}; // export const config = {
+//     host: "t2bkwyjck6bw.aws-sa-east-1-1.psdb.cloud",
+//     user: "a9dhb0t5ksso",
+//     password: "pscale_pw_J0CminD5flr1iZZDscllnICqVH_hUZ5lNDfirAdFUDc",
+//     database: "tasksdb",
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+// }
+
+exports.config = config;
--- routes/tasks.js
+++ routes/tasks.js
@@ -10,12 +10,12 @@ var _express = require("express");
 var _tasks = require("../controllers/tasks");
 
 var router = (0, _express.Router)();
-router.get('/', _tasks.home);
-router.get('/tasks/count');
-router.get('/tasks/:id');
+router.get('/tasks/count', _tasks.getTaskCount);
+router.get('/tasks/:id', _tasks.getTask);
 router.get('/tasks', _tasks.getTasks);
-router.post('/tasks');
-router["delete"]('/tasks/:id');
-router.put('/tasks/:id');
+router.post('/tasks', _tasks.saveTask);
+router["delete"]('/tasks/:id', _tasks.deleteTask);
+router.put('/tasks/:id', _tasks.updateTask);
+router.get('/', _tasks.home);
 var _default = router;
 exports["default"] = _default;
