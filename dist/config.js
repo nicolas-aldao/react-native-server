62ed8afc6a8501a29d6388fd78195a2df50bede0
Carlos Nicolás Aldao <caldao@baufest.com>
2022-06-23 18:39:14 -0300

    some changes 5

--- dist/config.js
+++ dist/config.js
@@ -10,23 +10,23 @@ var _dotenv = require("dotenv");
 var REACT_APP_DB_HOST = process.env.REACT_APP_DB_HOST;
 var REACT_APP_DB_USER = process.env.REACT_APP_DB_USER;
 var REACT_APP_PASSWORD = process.env.REACT_APP_PASSWORD;
-(0, _dotenv.config)(); // export const config = {
-//     host: REACT_APP_DB_HOST || 'localhost',
-//     user: REACT_APP_DB_USER || 'root',
-//     password: REACT_APP_PASSWORD || '',
-//     database: 'tasksdb' || 'test',
+(0, _dotenv.config)();
+var config = {
+  host: REACT_APP_DB_HOST || 'localhost',
+  user: REACT_APP_DB_USER || 'root',
+  password: REACT_APP_PASSWORD || '',
+  database: 'tasksdb' || 'test',
+  ssl: {
+    rejectUnauthorized: false
+  }
+}; // export const config = {
+//     host: "t2bkwyjck6bw.aws-sa-east-1-1.psdb.cloud",
+//     user: "215zzoe2gvw6",
+//     password: "pscale_pw_zacWK0FXw7xRT23iLbpVVmS-JhH4cTCiUkeCJgv9xXU",
+//     database: "tasksdb",
 //     ssl: {
 //         rejectUnauthorized: false
 //     }
 // }
 
-var config = {
-  host: "t2bkwyjck6bw.aws-sa-east-1-1.psdb.cloud",
-  user: "215zzoe2gvw6",
-  password: "pscale_pw_zacWK0FXw7xRT23iLbpVVmS-JhH4cTCiUkeCJgv9xXU",
-  database: "tasksdb",
-  ssl: {
-    rejectUnauthorized: false
-  }
-};
 exports.config = config;
--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,8 @@ var _app = _interopRequireDefault(require("./app"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var port = 3000;
+var port = process.env.PORT || 8080;
 
-_app["default"].listen(port);
-
-console.log('Server running on port ' + port);
+_app["default"].listen(port, function () {
+  console.log("app running on port 8080");
+});
